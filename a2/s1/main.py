afe8e130c877cf52f1fa6a07f7156b3c536bd3ff
cjardin <cjardin@jardinlabs.com>
2020-02-09 11:27:16 -0800

    Mr. Kaleb Narration File.

--- a2/s1/main.py
+++ a2/s1/main.py
@@ -1,13 +1,22 @@
+# Program Author:  Kaleb Newsom
+#         Course:  CS 351 Programming Languages
+#     Assignment:  Logic Programming Lab 2
+
+# import logic methods from kanren
 from kanren import run, var, membero
-import random
+import random                           # import package that creates random numbers
 
-x = list(range(1,70))
-y = list(range(1,27))
-random.shuffle(x)
-random.shuffle(y)
+x = list(range(1,70))                   # set x to a list of numbers from 1 to 69 inclusive
+y = list(range(1,27))                   # set y to a list of numbers from 1 to 26 inclusive
+random.shuffle(x)                       # randomly shuffle the list of numbers from 1 to 69 inclusive
+random.shuffle(y)                       # randomly shuffle the list of numbers from 1 to 26
 
-z = var()
+z = var()                               # let z be a logic variable
+# let user know that the following statement will be the first 5 powerball numbers
 print('These are your first 5 randomly chosen numbers to select on your powerball ticket!')
+# display a tupple containing the first 5 numbers of the randomized list of numbers including numbers 1 to 69 inclusive
 print(sorted(run(5, z, membero(z, x), membero(z, x), membero(z, x), membero(z, x), membero(z, x))))
+# let user know that the following statement will be the 6th powerball number
 print('This is your randomly chosen 6th powerball number to select on your powerball ticket!')
+# display a tupple containing the 6th powerball number
 print(sorted(run(1, z, membero(z, (y)))))
