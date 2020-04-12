f6a18c3e2dcf07b89df70da4edb2ac9d03c9d81d
Chris Do <do.chris023@gmail.com>
2020-04-12 14:33:59 -0700

    Add files via upload

---
+++ a1/do046/submit.php
@@ -0,0 +1,7 @@
+<?php   
+    print "Your name is ". $Name;   
+    print "<br />";   
+    print "You are ". $Age . " years old";   
+    print "<br />";   $old = 25 + $Age;
+    print "In 25 years you will be " . $old . " years old";   
+?>
